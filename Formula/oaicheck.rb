2703f2404232092691e6498d69ea4c991d258b97
Kevin Jiang <kevin51jiang@email.com>
2026-03-05 00:55:14 -0500

    oaicheck 0.3.0
    Created by https://github.com/kevin51jiang/oaicheck/actions/runs/22704460650

--- Formula/oaicheck.rb
+++ Formula/oaicheck.rb
@@ -1,8 +1,8 @@
 class Oaicheck < Formula
   desc "Simple CLI for testing OpenAI API configurations"
   homepage "https://github.com/kevin51jiang/oaicheck"
-  url "https://github.com/kevin51jiang/oaicheck/archive/refs/tags/main-2-a6ed907.tar.gz"
-  sha256 "8a081b85db66da84cdfaf5c9d9c425188f20c51d1e0f981fa43c82dd7ab0b32c"
+  url "https://github.com/kevin51jiang/oaicheck/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "7a5359d4e445d291493825d2cd10596bb6a46da996632eb0cb6474dcf10d2735"
   license "AGPL-3.0"
 
   depends_on "go" => :build
